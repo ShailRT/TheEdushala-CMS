71afe466592dc7c90b1a9aee80b01eff1a586f0c
ShailRT <shailesh18h@gmail.com>
2022-06-12 16:37:55 +0530

    main

--- cms/models.py
+++ cms/models.py
@@ -2,23 +2,6 @@ from codecs import backslashreplace_errors
 from string import digits
 from django.db import models
 
-class CollegeModel(models.Model):
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
-    photo = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    affilation = models.CharField(max_length=200)
-    information = models.TextField(blank=True, null=True)
-    courses = models.TextField(blank=True, null=True)
-    admission = models.TextField(blank=True, null=True)
-    placement = models.TextField(blank=True, null=True)
-    ranking = models.TextField(blank=True, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-date_added']
-
 class CourseModel(models.Model):
     name = models.CharField(max_length=200)
     overview = models.TextField(blank=True, null=True)
@@ -31,4 +14,31 @@ class CourseModel(models.Model):
 
     class Meta:
         ordering = ['-date_added']
+    
+    def __str__(self):
+        return self.name
 
+class CollegeModel(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    photo = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    affilation = models.CharField(max_length=200)
+    information = models.TextField(blank=True, null=True)
+    courses = models.TextField(blank=True, null=True)
+    courses_table = models.TextField(blank=True, null=True)
+
+    admission = models.TextField(blank=True, null=True)
+    placement = models.TextField(blank=True, null=True)
+    ranking = models.TextField(blank=True, null=True)
+    fees = models.TextField(blank=True, null=True)
+    eligibility = models.TextField(blank=True, null=True)
+    course = models.ManyToManyField(CourseModel, related_name='course')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.name
--- cms/views.py
+++ cms/views.py
@@ -3,14 +3,28 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import CollegeModel, CourseModel
 from lms.forms import LeadForm
+import re
 
 def college_page(request, college_slug):
     college = get_object_or_404(CollegeModel, slug = college_slug)
-    return render(request, 'college.html', {'college': college})
+    course_tab = re.split(' |\r\n', college.courses_table)
+    ct_course = []
+    ct_fees = []
+    ct = {}
+    current = ''
+    for i in range(len(course_tab)):
+        if i%2==0:
+            ct[course_tab[i]]= ''
+            current = course_tab[i]
+        else:
+            ct[current] = course_tab[i]
+
+    return render(request, 'college.html', {'college': college, 'ct_course':ct_course, 'ct_fees':ct_fees, 'ct': ct})
 
 def course_page(request, course_slug):
     course = get_object_or_404(CourseModel, slug=course_slug)
     new_course = CourseModel.objects.all()[0:8]
+    course_tab = re.split('-',course.courses_table)
     if request.method == 'POST':
         form = LeadForm(request.POST)
         if form.is_valid():
